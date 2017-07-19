6c4de277768db899ec3c87f3f7137e8337d331ef
Dimitri <dimitri.pauls@langtec.de>
2017-07-19 12:31:44 +0200

    Comments

--- alarms/parser.py
+++ alarms/parser.py
@@ -4,6 +4,11 @@ from pathlib import Path
 
 
 def add_date(date):
+    """
+    Hilfsfunktion zum Einlesen der Spalten 2 und 3 der Diagnose-Logs.
+    :param date: datetime-Objekt, das den Tag des Logs repräsentiert
+    :return: datetime mit Datums- und Zeitangabe
+    """
     def add_time(time_str):
         time = datetime.strptime(time_str, '%H:%M:%S').time()
         return datetime.combine(date, time)
@@ -11,6 +16,12 @@ def add_date(date):
 
 
 def read_csv(path):
+    """
+    Liest ein Diagnose-Logdatei im CSV-Format ein.
+    
+    :param path: Dateiname des Logs, z.B. "ALARMS_20110102.csv" 
+    :return: pandas-Dataframe mit den vier Spalten Element-ID, Start, Ende, Severity
+    """
     base = Path(path).name
 
     date = datetime.strptime(base, 'alarms_%Y%m%d.csv')
@@ -21,6 +32,11 @@ def read_csv(path):
 
 
 def read_dir(path):
+    """
+    Liest alle Diagnose-Logdateien in einem Verzeichnis ein.
+    :param path: Verzeichnisname, z.B. "." oder "C:\Logs\2016"
+    :return: pandas-Dataframe mit den vier Spalten Element-ID, Start, Ende, Severity
+    """
     p = Path(path)
 
     df = None
--- alarms/query.py
+++ alarms/query.py
@@ -2,13 +2,37 @@ from datetime import timedelta
 
 
 def get_alarms_for_element(df, element_id):
+    """
+    Extrahiert alle Alarm-Meldungen eines bestimmten Elements.
+    :param df: pandas-Dataframe mit den Spalten Element-ID, start, Ende, Severity
+    :param element_id: ID eines Elementes, z.b. 17
+    :type element_id: int
+    :return: gefilterter Dataframe
+    """
     return df[df['ELEMENT ID'] == element_id]
 
 
 def get_alarms_in_interval(df, start_time, end_time):
+    """
+    Extrahiert alle Alarm-Meldungen, die in ein Zeitintervall fallen.
+
+    :param df: pandas-Dataframe mit den Spalten Element-ID, start, Ende, Severity
+    :param start_time: datetime-Objekt der Anfangszeit 
+    :param end_time:   datetime-Objekt der Endzeit
+    :return: alle Alarm-Meldungen, die sich ganz oder teilweise mit dem Intervall überschneiden.  
+    """
     return df[(start_time <= df['START']) & (df['START'] <= end_time)
               | (start_time <= df['end']) & (df['end'] <= end_time)]
 
 
 def sum_alarm_durations(df):
+    """
+    Addiert die Dauer aller genannten Alarm-Meldungen.
+    
+    :param df: pandas-Dataframe mit den Spalten Element-ID, start, Ende, Severity
+    :rtype:  timedelta
+    :return  Summe der Dauern aller genannten Alarme. Wenn verschiedene Meldungen einander
+     überschneiden, wird die gemeinsame Dauer beider Meldungen nicht subtrahiert, 
+      d.h. das Ergebnis kann größer sein als der Gesamtzeitraum.
+    """
     return sum((row['end'] - row['START'] for _, row in df.iterrows()), timedelta())
