f3fbe4a3e7a0a2f538d90dd122bfce57c77f94de
Roerd Hinrichsen <roerd.hinrichsen@langtec.de>
2017-07-19 12:18:00 +0200

    Fix time intervals

--- alarms/query.py
+++ alarms/query.py
@@ -1,6 +1,14 @@
+from datetime import timedelta
+
+
 def get_alarms_for_element(df, element_id):
     return df[df['ELEMENT ID'] == element_id]
 
 
 def get_alarms_in_interval(df, start_time, end_time):
-    return df[start_time <= df['START'] <= end_time or start_time <= df['end'] <= end_time]
+    return df[(start_time <= df['START']) & (df['START'] <= end_time)
+              | (start_time <= df['end']) & (df['end'] <= end_time)]
+
+
+def sum_alarm_durations(df):
+    return sum((row['end'] - row['START'] for _, row in df.iterrows()), timedelta())
